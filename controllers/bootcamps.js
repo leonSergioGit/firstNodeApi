7daeaabec848f81f6ca352e5bc7931c59215a012
Sergio Leon <leongit1990@gmail.com>
2019-11-15 12:41:42 +0100

    Added advanced queries

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,54 @@ const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
+//REQ.QUERY very useful feature of express to make advanced queries. 
+
+
 // @desc    Get all bootcamps
 // @route   Get /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps =  asyncHandler(async (req, res, next) => {
-        const bootcamps = await Bootcamp.find();
+        let query;
+
+        //Copy req.query
+        const reqQuery = { ...req.query };
+
+        //Fields to exclude
+        const removeFields = ['select', 'sort'];
+
+        //Loop over removeFields and delete them from reqQuery
+        removeFields.forEach(param => delete reqQuery[param]);
+
+        
+        //Create query String
+        let queryStr = JSON.stringify(reqQuery);
+
+        //Create operators($gt, $gte, etc)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+        //Finding resource
+        query = Bootcamp.find(JSON.parse(queryStr));
+
+        //SELECT FIELDS
+        if(req.query.select){
+            const fields = req.query.select.split(',').join(' ');
+            query = query.select(fields);
+        }
+
+        //Sort
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        //Executing query
+        const bootcamps = await query;
 
         res
          .status(200)
-        .json({ success: true, count: bootcamps.length, data: bootcamps });
+         .json({ success: true, count: bootcamps.length, data: bootcamps });
 
 })
 
