9d3663d971c23bb265464d932dbedda5ab063993
unknown <leongit1990@gmail.com>
2019-12-27 10:53:09 +0100

    CRUD functionality for users (only admin)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
@@ -64,6 +65,48 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     });
 });
 
+// @desc    Update user details
+// @route   PUT /api/v1/auth/updatedetailes
+// @access  Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+    const fieldsToUpdate = {
+        name: req.body.name,
+        email: req.body.email
+    }
+
+    const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+// @desc    Update password
+// @route   Put /api/v1/auth/updatepassword
+// @access  Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+
+    //Check current password
+    if(!(await user.matchPassword(req.body.currentPassword))){
+        return next(new ErrorResponse('Password is incorrect', 401));
+    }
+
+    user.password = req.body.newPassword;
+    await user.save();
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+
+    sendTokenResponse(user, 200, res);
+});
+
 
 // @desc    Forgot pàssword
 // @route   POST /api/v1/auth/forgotpassword
@@ -76,12 +119,12 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     }
 
     //Get reset token
-    const resetToken = user.getResetPasswordToken();
+    const resettoken = user.getResetPasswordToken();
 
     await user.save({ validateBeforeSave:  false });
 
     //Create reset url
-    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetPassword/${resettoken}`;
 
     const message = `You are receiving this email because it has been requested the reset of a password. PLease
         make a put request to: \n\n ${resetUrl}`;
@@ -108,6 +151,36 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
         success: true,
         data: user
     });
+});
+
+
+// @desc    Reset password
+// @route   PUT /api/v1/auth/resetpassword/:resettoken
+// @access  Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+    //Get hashed token
+    const resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(req.params.resettoken)
+        .digest('hext');
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    });
+
+    if(!user){
+        return next(new ErrorResponse('Invalid token', 400));
+    }
+
+    //Set new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
 });
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword } = require('../controllers/auth');
+const { register, 
+        login, 
+        getMe, 
+        forgotPassword, 
+        resetPassword, 
+        updateDetails,
+        updatePassword
+    } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -8,7 +15,9 @@ const { protect } = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
-router.post('/forgotPassword', forgotPassword)
-
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
+router.post('/forgotPassword', forgotPassword);
+router.put('/resetPassword/:resettoken', resetPassword);
 
 module.exports = router;
